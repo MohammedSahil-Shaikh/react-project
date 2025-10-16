f8e17eee06bf2d936e68bddd5c39475d8ec48b9b
MohammedSahil-Shaikh <sahiliqbalshaikh789@gmail.com>
2025-10-16 23:27:07 +0530

    'episode-2'

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement(
 //     'h1',
 //     {
@@ -9,11 +12,13 @@
 
 // console.log('heading: ', heading);
 
+// "main": "app.js",
+
 const parent = React.createElement('div', { id: 'parent' },
     [
         React.createElement('div', { id: 'child' },
             [
-                React.createElement('h1', {}, 'I am an h1 tag'),
+                React.createElement('h1', {}, 'This is N.React '),
                 React.createElement('h2', {}, 'I am an h2 tag'),
             ]
         ),
