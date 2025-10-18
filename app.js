58f3aa42b02555973e04b4c085d8f8b997ae9809
MohammedSahil-Shaikh <sahiliqbalshaikh789@gmail.com>
2025-10-18 19:47:43 +0530

    Assignment Ep-3

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client'
 
+const reactHeader = React.createElement('div', { className: 'title' },
+    [
+        React.createElement('h1', {}, 'I am h1 tag by React'),
+        React.createElement('h2', {}, 'I am h2 tag by React'),
+        React.createElement('h3', {}, 'I am h3 tag by React'),
+    ]
+)
 
-const Heading = () => (
-    <div>
-        {Title()}
-        <Title />
-        <h1>Heading from Functional Component 🚀</h1>
+const jsxHeader = (
+    <div className="title">
+        <h1>I am h1 tag by JSX.</h1>
+        <h2>I am h2 tag by JSX</h2>
+        <h3>I am h3 tag by JSX.</h3>
     </div>
 )
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<Heading />);
-
-const Title = () => <h3>Hello</h3>;
-
-const title = (
-    <div>
-        <h3>Hello I am const title</h3>
-        <h3>How are you</h3>
-        {/* <Heading /> */}
+const JSXHeader = () => (
+    <div className="title">
+        <h1>I am h1 tag by JSx in a Function Component</h1>
+        <h2>I am h2 tag by JSX in a Function Component</h2>
+        <h3>I am h3 tag by JSX in a Function Component.</h3>
     </div>
 )
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <div>
+        {reactHeader}
+        {jsxHeader}
+        <JSXHeader />
+    </div>
+
+);
