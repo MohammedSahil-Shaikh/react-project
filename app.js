7477d5f004d05eb1a6ff1a8c053762aa55d5d38d
MohammedSahil-Shaikh <sahiliqbalshaikh789@gmail.com>
2025-10-18 18:18:44 +0530

    Ep-3 Part-4

--- app.js
+++ app.js
@@ -2,35 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-//React.createElement => React Element(which is a JS Object) => HTML Element (React Element is converted to HTML element using render method) 
-// const heading = React.createElement('h1', {id: 'head'}, 'Hello React 🚀');
-
-
-//JSX is transpiled before it reaches the JavaScript engine by Parcel and Babel.
-//JSX => React.createElement (using babel the JSX Element is converted to React Element) => React Element (JS Object) => HTML ELement (using render) 
-// const jsxHeading = <h1>Hello React from JSX 🚀</h1>
-
-//Functional Component - Type 1
-const Heading1 = () => <h1>Heading from Functional Component 1</h1>
-
-//Functional Components Definition
-// It is just a JavaScript Function that returns some JSX or a
-// react element.
-
-//Functional Component - Type 2
-const Heading2 = () => (
+const Heading = () => (
     <div>
-        <Heading1 />
-        <h1>Heading from Functional Component 2</h1>
-        <Heading3 />
+        {Title()}
+        <Title />
+        <h1>Heading from Functional Component 🚀</h1>
     </div>
 )
 
-//Functional Component - Type 3
-const Heading3 = () => {
-    return <h1>Heading from Functional Component 3</h1>
-}
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
+root.render(<Heading />);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Heading2 />);
+const Title = () => <h3>Hello</h3>;
+
+const title = (
+    <div>
+        <h3>Hello I am const title</h3>
+        <h3>How are you</h3>
+        {/* <Heading /> */}
+    </div>
+)
